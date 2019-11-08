54fa9ab4ad35c9a74cf3cf3d67fe7aebb3acd27f
Christian Treffs <christian.treffs@7p-group.com>
2019-11-08 23:07:27 +0100

    Running in Swift

--- breakout/Breakout10x10.playground/Contents.swift
+++ breakout/Breakout10x10.playground/Contents.swift
@@ -7,6 +7,8 @@ let HEIGHT = 9
 let EMPTY: FIELD = 0
 let BALL: FIELD = 1
 let PLATE: FIELD = 2
+
+
 typealias BOARD = [FIELD]
 
 func makeBoard(_ board: inout BOARD) {
@@ -47,17 +49,52 @@ func movePlateRight(_ board: inout BOARD) {
 
 func setField(_ board: inout BOARD, _ x: Int, _ y: Int, _ value: FIELD) {
     assert(x < WIDTH && y < HEIGHT)
-    let idx = y * WIDTH + x
-    board[idx] = value
+    board[index(x,y)] = value
 }
 
 func getField(_ board: BOARD, _ x: Int, _ y: Int) -> FIELD {
     assert(x < WIDTH && y < HEIGHT)
-    let idx = y * WIDTH + x
-    return board[idx]
+    return board[index(x,y)]
 }
 
-func draw(_ board: BOARD) {
+func index(_ x: Int, _ y: Int) -> Int {
+    return y * WIDTH + x
+}
+
+func nextX(_ x: Int) -> Int {
+    return (x+1) % WIDTH
+}
+
+func nextY(_ y: Int) -> Int {
+    return (y+1) % HEIGHT
+}
+
+nextX(0)
+nextX(1)
+nextX(2)
+nextX(3)
+nextX(4)
+nextX(5)
+nextX(6)
+nextX(7)
+nextX(8)
+nextX(9)
+nextX(10)
+nextX(11)
+nextX(12)
+
+var x = 0
+var y = 0
+func draw(_ board: inout BOARD) {
+    
+    board[index(x, 1)] = 0
+    x = nextX(x)
+    board[index(x, 1)] = 5
+    
+    board[index(2, y)] = 0
+    y = nextY(y)
+    board[index(2, y)] = 7
+    
     var out: String = ""
     for y in 0..<HEIGHT {
         for x in 0..<WIDTH {
@@ -69,29 +106,31 @@ func draw(_ board: BOARD) {
 }
 
 
+
+
 var board: BOARD = BOARD()
 makeBoard(&board)
 
-draw(board)
+draw(&board)
 movePlateLeft(&board)
-draw(board)
+draw(&board)
 movePlateLeft(&board)
-draw(board)
+draw(&board)
 movePlateLeft(&board)
-draw(board)
+draw(&board)
 movePlateLeft(&board)
-draw(board)
+draw(&board)
 movePlateRight(&board)
-draw(board)
+draw(&board)
 movePlateRight(&board)
-draw(board)
+draw(&board)
 movePlateRight(&board)
-draw(board)
+draw(&board)
 movePlateRight(&board)
-draw(board)
+draw(&board)
 movePlateRight(&board)
-draw(board)
+draw(&board)
 movePlateRight(&board)
-draw(board)
+draw(&board)
 movePlateRight(&board)
-draw(board)
+draw(&board)
